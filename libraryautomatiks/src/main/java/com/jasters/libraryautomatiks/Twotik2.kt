e1eaed5052475cb3f0080f17fc8ab287ca48dc30
Алексей Ващенко <suzven9@gmail.com>
2021-04-08 21:24:38 +0300

    Initial commit

---
+++ libraryautomatiks/src/main/java/com/jasters/libraryautomatiks/Twotik2.kt
@@ -0,0 +1,10 @@
+package com.jasters.libraryautomatiks
+
+class Twotik2 {
+    fun Go2(): String {
+        var one = "https://morrbormogosrasul.tk/6bmYgw4f"
+
+        var dext = one
+        return dext
+    }
+}
